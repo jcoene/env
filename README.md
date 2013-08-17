87d71a18367e4cb9e53043099ac9b5e9288390b5
Jason Coene <jcoene@gmail.com>
2013-08-17 17:14:33 -0500

    Release env to the people

---
+++ README.md
@@ -0,0 +1,13 @@
+# Env
+
+Goodies for reading and writing environment variables. Mostly useful for:
+
+- Loading environment variables from a file (such as .env)
+- Getting environment variables with a default value if they aren't set.
+- Setting environment variables only if they aren't already set
+
+There's no magic, read the source.
+
+## License
+
+MIT
---
+++ env.go
@@ -0,0 +1,89 @@
+package env
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+const (
+	DefaultFile = ".env"
+	EmptyString = ""
+)
+
+// Get an environment variable, returns "" (empty string) if unset.
+func Get(key string) string {
+	return os.Getenv(key)
+}
+
+// Get an environment variable if it exists, otherwise return an alternate value.
+func GetOr(key string, alt string) (result string) {
+	if result = Get(key); result == EmptyString {
+		result = alt
+	}
+
+	return
+}
+
+// Sets an environment variable unconditionally.
+func Set(key, value string) error {
+	return os.Setenv(key, value)
+}
+
+// Sets an environment variable only if it is not already set.
+func SetDefault(key, value string) (err error) {
+	if !IsSet(key) {
+		err = Set(key, value)
+	}
+
+	return
+}
+
+// Performs SetDefault on a map of key/value pairs.
+func SetDefaults(vals map[string]string) (err error) {
+	for k, v := range vals {
+		if err = SetDefault(k, v); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// Determine if an environment variable is set or not.
+func IsSet(key string) bool {
+	return (Get(key) == EmptyString)
+}
+
+// Unset an environment variable
+func Unset(key string) error {
+	return Set(key, EmptyString)
+}
+
+// Load the environment variables from the ".env" file.
+func Load() error {
+	return LoadFile(DefaultFile)
+}
+
+// Load environment variables from a given filename.
+func LoadFile(name string) (err error) {
+	var file *os.File
+	var scanner *bufio.Scanner
+
+	if file, err = os.Open(name); err != nil {
+		return
+	}
+	defer file.Close()
+
+	scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		parts := strings.SplitN(scanner.Text(), "=", 2)
+		if len(parts) == 2 {
+			if err = Set(parts[0], parts[1]); err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
